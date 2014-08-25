5876f31a6c5dde21afc4f82e5762f0d86f46ebc3
Néstor Salceda <nestor.salceda@gmail.com>
2014-08-25 10:49:10 +0200

    Migrate to numo theme

---
+++ _includes/javascript.js
@@ -0,0 +1,2 @@
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/0.9.9/jquery.magnific-popup.min.js"></script>
